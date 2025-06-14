88e910b3b0412619d5feea0a0326a35114357b85
Sumit_Zende <sumitzende652@gmail.com>
2025-06-14 10:28:06 +0530

    clock desinged work properly

--- DigitalClock/src/App.jsx
+++ DigitalClock/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const FormateedTime = time.toLocaleTimeString();
+  const FomrateedDate = time.toDateString();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="clock-wrapper">
+        <h1>🕒 Digital Clock</h1>
+        <div className="clock">
+          <div className="display">
+            <div className="timer">
+              <h3>{FormateedTime}</h3>
+            </div>
+            <div className="date">
+              <h3>{FomrateedDate}</h3>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
