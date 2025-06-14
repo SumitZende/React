c273abc45295cd60e5d6de10d9a98960306a41f7
Sumit_Zende <sumitzende652@gmail.com>
2025-06-14 06:17:59 +0530

    Functional Part is Compeleted

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick=()=>{
+      setCount(count+1)
+  }
+
+  const handleRest=()=>{
+    setCount(0)
+  }
+
   return (
     <>
-      
+      <div className='body'>
+          <h1>Counter App</h1>
+      <hr />
+        <div className='click'>
+          <button onClick={handleClick}>Click Me</button>
+          <button onClick={handleRest}>Reset</button>
+        </div>
+        <div className='count'>
+        <p>You clicked {count} times</p>
+        </div>
+      </div>
     </>
   )
 }
